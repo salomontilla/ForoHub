be646758f066d259618da622de6cefc3d2984207
salo <salomontilla@outlook.com>
2024-12-18 11:40:05 -0500

    create topic constructor using dto

--- src/main/java/alura/salo/foroHub/model/Topic.java
+++ src/main/java/alura/salo/foroHub/model/Topic.java
@@ -24,4 +24,13 @@ public class Topic {
 
     @Enumerated(EnumType.STRING) @Column(name = "curse")
     private Curse curse;
+
+    public Topic(TopicDTO topicDTO) {
+        this.title = topicDTO.title();
+        this.message = topicDTO.message();
+        this.creationDate = topicDTO.creationDate();
+        this.status = topicDTO.status();
+        this.autor = topicDTO.autor();
+        this.curse = topicDTO.curse();
+    }
 }
