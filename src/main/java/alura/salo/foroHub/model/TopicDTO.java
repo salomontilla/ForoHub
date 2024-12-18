ffbe3343b2978c5e2a1b5bdcfb6e3f15fe2a2e25
salo <salomontilla@outlook.com>
2024-12-18 11:37:11 -0500

    validate topic DTO class

--- src/main/java/alura/salo/foroHub/model/TopicDTO.java
+++ src/main/java/alura/salo/foroHub/model/TopicDTO.java
@@ -3,15 +3,23 @@ package alura.salo.foroHub.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record TopicDTO(
+        @NotBlank
         String title,
+        @NotBlank
         String message,
+        @NotNull
         LocalDateTime creationDate,
+        @NotBlank
         boolean status,
+        @NotBlank
         String autor,
+        @NotBlank
         Curse curse
 ) {
 }
