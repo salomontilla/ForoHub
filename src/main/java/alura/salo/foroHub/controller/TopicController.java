596e50112c823beb249c6dfd24fcbe67290cf9bb
salo <salomontilla@outlook.com>
2024-12-18 12:05:55 -0500

    create post method to create topics

--- src/main/java/alura/salo/foroHub/controller/TopicController.java
+++ src/main/java/alura/salo/foroHub/controller/TopicController.java
@@ -1,17 +1,33 @@
 package alura.salo.foroHub.controller;
 
+import alura.salo.foroHub.model.Topic;
 import alura.salo.foroHub.model.TopicDTO;
+import alura.salo.foroHub.model.TopicResponseDTO;
+import alura.salo.foroHub.repository.TopicRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/topics")
 public class TopicController {
+    @Autowired
+    TopicRepository topicRepository;
+
     @PostMapping
-    public void createTopic(@RequestBody @Valid TopicDTO topicDTO) {
-        System.out.println("Creating a new topic");
+    public ResponseEntity<TopicResponseDTO> createTopic(@RequestBody @Valid TopicDTO topicDTO, UriComponentsBuilder uriBuilder) {
+
+        Topic newTopic = topicRepository.save(new Topic(topicDTO));
+        TopicResponseDTO topicResponseDTO = new TopicResponseDTO(newTopic.getId(), newTopic.getTitle(), newTopic.getMessage(),
+                newTopic.getCreationDate(), newTopic.getStatus(), newTopic.getAutor(), newTopic.getCurse());
+        URI url = uriBuilder.path("/topics/{id}").buildAndExpand(newTopic.getId()).toUri();
+        return ResponseEntity.created(url).body(topicResponseDTO);
     }
 }
