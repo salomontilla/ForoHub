7b683ffee42fb36e8972d33be81cda9dad765579
salo <salomontilla@outlook.com>
2024-12-18 12:11:32 -0500

    delete creation date request field from the json

--- src/main/java/alura/salo/foroHub/model/TopicDTO.java
+++ src/main/java/alura/salo/foroHub/model/TopicDTO.java
@@ -1,17 +1,11 @@
 package alura.salo.foroHub.model;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-import java.time.LocalDateTime;
-
 public record TopicDTO(
         @NotBlank
         String title,
         @NotBlank
         String message,
-        @NotNull
-        LocalDateTime creationDate,
         @NotBlank
         boolean status,
         @NotBlank
