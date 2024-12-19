1c9ec4b6d906f4fea05b2ef7b754dc74d5416aa3
salo <salomontilla@outlook.com>
2024-12-19 01:53:50 -0500

    create token service class

--- src/main/java/alura/salo/foroHub/infra/security/TokenService.java
+++ src/main/java/alura/salo/foroHub/infra/security/TokenService.java
@@ -1,4 +1,46 @@
 package alura.salo.foroHub.infra.security;
 
+import alura.salo.foroHub.model.UserModel;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Service
 public class TokenService {
+    @Value("${foroHub.security.secret}")
+    private String apiSecret;
+
+    public String generateToken(UserModel user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            return JWT.create()
+                    .withIssuer("Alura Project")
+                    .withSubject(user.getUsername())
+                    .withClaim("id", user.getId())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException(exception);
+        }
+    }
+
+    public Instant generateExpirationDate() {
+        return Instant.now().plus(1, ChronoUnit.HOURS);
+    }
+    public String getSubject(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            return JWT.require(algorithm)
+                    .withIssuer("Voll Med")
+                    .build().verify(token).getSubject();
+
+        } catch (JWTCreationException exception){
+            throw new RuntimeException(exception);
+        }
+    }
 }
